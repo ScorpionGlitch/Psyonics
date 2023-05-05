b5a4e393fbe6ef2c6c8433f1e21d34ac1e1e0000
David Allen Archer <David.Allen.Archer@gmail.com>
2023-05-05 15:42:37 -0400

    [Issue-00002] Added Interaction Interface and tested functionality with NPC and Dumby (enemy)

--- Source/Psyonic/Private/InteractionSystem/ActorComponents/InteractionComponent.cpp
+++ Source/Psyonic/Private/InteractionSystem/ActorComponents/InteractionComponent.cpp
@@ -3,32 +3,12 @@
 
 #include "Public/InteractionSystem/ActorComponents/InteractionComponent.h"
 
-// Sets default values for this component's properties
 UInteractionComponent::UInteractionComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
 void UInteractionComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
-
-
-// Called every frame
-void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
--- Source/Psyonic/Public/InteractionSystem/ActorComponents/InteractionComponent.h
+++ Source/Psyonic/Public/InteractionSystem/ActorComponents/InteractionComponent.h
@@ -6,12 +6,25 @@
 #include "Components/ActorComponent.h"
 #include "InteractionComponent.generated.h"
 
-
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PSYONIC_API UInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText ActionText1;
+	UPROPERTY(BlueprintReadOnly, EditInstanceOnly)
+	FText ActionText2;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	FText ActionText3;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FText ActionText4;
+	UPROPERTY(BlueprintReadWrite, EditInstanceOnly)
+	FText ActionText5;
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	FText ActionText6;
+	
 public:	
 	// Sets default values for this component's properties
 	UInteractionComponent();
@@ -19,10 +32,4 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 };
